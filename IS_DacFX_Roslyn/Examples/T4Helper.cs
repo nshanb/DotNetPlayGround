7a4dcf32c1dcd6e8071f2f9d63136a6474c9ddbb
Nshan Barseghyan <User@TOSHIBA>
2017-01-12 02:31:26 +0400

    test on other comp

---
+++ IS_DacFX_Roslyn/Examples/T4Helper.cs
@@ -0,0 +1,8 @@
+﻿namespace Examples
+{
+    public class T4Helper
+    {
+		public static string TemplateDir = @"C:\Users\User\Documents\Visual Studio 2015\Projects\DotNetPlayGround\IS_DacFX_Roslyn\Examples";
+    }
+}
+
