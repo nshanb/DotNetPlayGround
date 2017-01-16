31e25069c4dd5310cc1b95feebef45b2af7b596c
Nshan Barseghyan <User@TOSHIBA>
2017-01-17 00:44:49 +0400

    logconfigs examples project

---
+++ NLogTest/UsefullConfigs/SimpleConfigs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NLog;
+
+namespace UsefullConfigs
+{
+    [TestClass]
+    public class SimpleConfigs
+    {
+        [TestMethod]
+        public void SimpleLog()
+        {
+            NLog.Config.XmlLoggingConfiguration xml = new NLog.Config.XmlLoggingConfiguration(@"..\..\Simple.config");
+            LogManager.Configuration = xml;
+            Logger SimpleLogger = LogManager.GetLogger("Simple");
+            SimpleLogger.Trace("This is a Trace");
+            SimpleLogger.Warn("This is a Warning");
+            try
+            {
+                int j = 0;
+                int i = 1 / j;
+            }
+            catch(Exception ex)
+            {
+                SimpleLogger.Error(ex,"This is an Error");
+            }
+        }
+    }
+}
