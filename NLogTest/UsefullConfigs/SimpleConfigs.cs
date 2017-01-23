709554c4d2f584dd2b165ec7aba32012e1a5712e
nshan.barseghyan <nshan.barseghyan@PC-0609>
2017-01-23 18:54:40 +0400

    Nlog config example

--- NLogTest/UsefullConfigs/SimpleConfigs.cs
+++ NLogTest/UsefullConfigs/SimpleConfigs.cs
@@ -20,10 +20,23 @@ namespace UsefullConfigs
                 int j = 0;
                 int i = 1 / j;
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                SimpleLogger.Error(ex,"This is an Error");
+                SimpleLogger.Error(ex, "This is an Error");
             }
+        }
+        [TestMethod]
+        public void LogByLoggerName()
+        {
+            NLog.LayoutRenderers.LayoutRenderer.Register("GlobalSequenceID", (logEvent) => logEvent.SequenceID);
+            NLog.Config.XmlLoggingConfiguration xml = new NLog.Config.XmlLoggingConfiguration(@"..\..\Simple1.config");
+            LogManager.Configuration = xml;
+            Logger SimpleLogger = LogManager.GetLogger("Simple");
+            Logger SimpleLogger1 = LogManager.GetLogger("SimpleAnother");
+            SimpleLogger.Trace("This is a Trace");
+            SimpleLogger.Warn("This is a Warning");
+            SimpleLogger1.Trace("This is a Trace");
+            SimpleLogger1.Warn("This is a Warning");
         }
     }
 }
