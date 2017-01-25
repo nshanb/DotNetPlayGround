07e056980ecf4afdfe16ee8cbc0e7c72fd094e71
nshan.barseghyan <nshan.barseghyan@PC-0609>
2017-01-25 19:09:18 +0400

    ExamineMST

---
+++ IS_DacFX_Roslyn/Examples/ExamineMSTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Examples
+{
+    [TestClass]
+    public class ExamineMSTest
+    {
+        // order => constructor, [TestInitialize], [TestMethod], [TestCleanup]
+        //
+        //
+        static Common.IMyLogger logger = Common.MyLogger.CommonLog;
+        public ExamineMSTest()
+        {
+            // class is constructed for each TestMethod
+            logger.Trace("From constructor");
+        }
+        [TestMethod]
+        public void TraceMethodCallSequence1()
+        {
+            logger.Trace("From [TestMethod] TraceMethodCallSequence1");
+        }
+        [TestMethod]
+        public void TraceMethodCallSequence2()
+        {
+            logger.Trace("From [TestMethod] TraceMethodCallSequence2");
+        }
+        [TestInitialize]
+        public void Init()
+        {
+            logger.Trace("From [TestInitialize]");
+        }
+        [TestCleanup]
+        public void Clean()
+        {
+            logger.Trace("From [TestCleanup]");
+        }
+    }
+}
