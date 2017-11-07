6eb7d1f8baf63bded3f9c3c3e0905d62a7fccd79
Nshan Barseghyan <User@TOSHIBA>
2017-11-07 08:53:00 +0400

    f# match examples

---
+++ FsharpPlayGround/FSConsole/MatchExamples.fs
@@ -0,0 +1,18 @@
+﻿module MatchExamples
+
+let example1 x = 
+    match x=1 with
+    | true -> "a" 
+    | false -> "b"
+
+let example1Better x = 
+    match x with
+    | 1 -> "a" 
+    | _ -> "b"
+
+let rec printList x = 
+    match x with
+    | [] -> printfn ""
+    | head::tail -> printfn "%d " head; printList tail
+
+
--- FsharpPlayGround/FSConsole/Program.fs
+++ FsharpPlayGround/FSConsole/Program.fs
@@ -8,6 +8,18 @@ let knapsackLight value1 weight1 value2 weight2 maxW =
  if r.IsEmpty then 0
  else fst r.[0]
 
+let arithmeticExpression a b c =
+    let rec res l (a,b,c) = 
+        match l with
+        | [] -> false
+        | head::tail when a head b = c -> true
+        | head::tail -> res tail (a,b,c)
+    res [(+),(-),(*),(/)] (a,b,c)
+
+// let oper xop (x1, x2, x3) = x1 xop x2 = x3
+// let r = for op in [(+),(-),(*),(/)] do oper op (a, b, c) 
+
+
 let test1 x = 1
 
 [<EntryPoint>]
